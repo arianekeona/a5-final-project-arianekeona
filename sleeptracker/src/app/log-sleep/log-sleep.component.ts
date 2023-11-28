d2cf7be848d98614d40b3651269dd8ce696232b0
arianekeona <akochoa@uci.edu>
2023-11-27 17:25:21 -0800

    Add log-sleep as a component.

---
+++ sleeptracker/src/app/log-sleep/log-sleep.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-log-sleep',
+  templateUrl: './log-sleep.component.html',
+  styleUrls: ['./log-sleep.component.scss'],
+})
+export class LogSleepComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
